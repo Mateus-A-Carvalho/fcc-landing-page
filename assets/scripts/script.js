ee3a97ddaa11b2551ab725583bf9d8df38b66ed9
Mateus-A-Carvalho <mateusabreu94@hotmail.com>
2024-02-23 12:52:12 -0300

    feat: started to work with ScrollReveal library to animate elements in page.

---
+++ assets/scripts/script.js
@@ -0,0 +1,3 @@
+ScrollReveal().reveal('.heading-presentation', { delay: 200});
+ScrollReveal().reveal('.description-container:nth-of-type(1)' ,{ distance: '-200px'});
+ScrollReveal().reveal('.description-container:nth-of-type(2)' ,{ distance: '200px'});
