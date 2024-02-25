6fe38dea4e923c68a6cf26ac255b8261bd49c806
Mateus-A-Carvalho <mateusabreu94@hotmail.com>
2024-02-24 21:35:08 -0300

    style: trying to fix nav-link :active state problem

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,6 @@
+// Using ScrollReveal Library
 ScrollReveal().reveal('.description-container:nth-of-type(1)' ,{ distance: '100px'});
 ScrollReveal().reveal('.description-container:nth-of-type(2)' ,{ distance: '100px'});
 ScrollReveal().reveal('.description-container:nth-of-type(1)' ,{ origin: 'left'});
 ScrollReveal().reveal('.description-container:nth-of-type(2)' ,{ origin: 'right'});
+// Using ScrollReveal Library
