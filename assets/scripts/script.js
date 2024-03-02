471c251bb4509ae6887af61924236d3ec216407f
Mateus-A-Carvalho <mateusabreu94@hotmail.com>
2024-03-01 23:13:00 -0300

    fix: fixing postion of products__name in ScrollReveal

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,4 +3,13 @@ ScrollReveal().reveal('.description-container:nth-of-type(1)' ,{ distance: '100p
 ScrollReveal().reveal('.description-container:nth-of-type(2)' ,{ distance: '100px'});
 ScrollReveal().reveal('.description-container:nth-of-type(1)' ,{ origin: 'left'});
 ScrollReveal().reveal('.description-container:nth-of-type(2)' ,{ origin: 'right'});
+
+ScrollReveal().reveal('.products__name' ,{ 
+  distance: '100px',
+  origin: 'right'
+});
+
+// Set the fourth, fifth and sixth products__name with other distance
+
+
 // Using ScrollReveal Library
