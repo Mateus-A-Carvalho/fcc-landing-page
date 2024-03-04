2f036542d26e8615a4338cd43cc1c242bd02b353
Mateus-A-Carvalho <mateusabreu94@hotmail.com>
2024-03-04 12:52:20 -0300

    style: still trying to fix animations problems with ScrollReveal

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,37 +1,24 @@
 // Using ScrollReveal Library
-ScrollReveal().reveal('.description-container:nth-of-type(1)' ,{ distance: '100px'});
-ScrollReveal().reveal('.description-container:nth-of-type(2)' ,{ distance: '100px'});
-ScrollReveal().reveal('.description-container:nth-of-type(1)' ,{ origin: 'left'});
-ScrollReveal().reveal('.description-container:nth-of-type(2)' ,{ origin: 'right'});
 
-ScrollReveal().reveal('.products__name' ,{ 
+// Animating the descriptions containers;
+ScrollReveal().reveal('.description-container:nth-of-type(1)' ,{ 
+  distance: '100px',
+  origin: 'left'
+});
+
+ScrollReveal().reveal('.description-container:nth-of-type(2)' ,{ 
   distance: '100px',
   origin: 'right'
 });
 
-ScrollReveal().reveal('.product-descriptor' ,{ 
+// Animating products section
+
+ScrollReveal().reveal('.products-img__container-wrapper:nth-of-type(1)' ,{ 
+  distance: '100px',
+  origin: 'left'
+});
+
+ScrollReveal().reveal('.products-img__container-wrapper:nth-of-type(2)' ,{ 
   distance: '100px',
   origin: 'right'
 });
-
-ScrollReveal().reveal('.fourth' ,{ 
-  distance: '100px',
-  origin: 'left'
-});
-
-ScrollReveal().reveal('.fifth' ,{ 
-  distance: '100px',
-  origin: 'left'
-});
-
-ScrollReveal().reveal('.sixth' ,{ 
-  distance: '100px',
-  origin: 'left'
-});
-
-
-
-// Set the fourth, fifth and sixth products__name with other distance
-
-
-// Using ScrollReveal Library
