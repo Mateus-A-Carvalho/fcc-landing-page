d7663b0c2a7f9a67f1d0478324aa498e3e35d2b9
Mateus-A-Carvalho <mateusabreu94@hotmail.com>
2024-03-05 12:37:32 -0300

    style: I decided to change the settings of ScrollReveal.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -15,10 +15,20 @@ ScrollReveal().reveal('.description-container:nth-of-type(2)' ,{
 
 ScrollReveal().reveal('.products-img__container-wrapper:nth-of-type(1)' ,{ 
   distance: '100px',
-  origin: 'left'
+  origin: 'top'
 });
 
 ScrollReveal().reveal('.products-img__container-wrapper:nth-of-type(2)' ,{ 
   distance: '100px',
-  origin: 'right'
+  origin: 'top'
+});
+
+ScrollReveal().reveal('.products__name' ,{ 
+  distance: '100px',
+  origin: 'bottom'
+});
+
+ScrollReveal().reveal('.product-descriptor' ,{ 
+  distance: '100px',
+  origin: 'bottom'
 });
