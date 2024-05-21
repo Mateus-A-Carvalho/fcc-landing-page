c9ddc6e684b16661c047525a7e8c8698fa3bd363
Mateus-A-Carvalho <mateusabreu94@hotmail.com>
2024-05-21 19:58:33 -0300

    style: finished the fieldset styling.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,34 +1,5 @@
-// Using ScrollReveal Library
+const submitBtn = document.querySelector('#submit');
 
-// Animating the descriptions containers;
-ScrollReveal().reveal('.description-container:nth-of-type(1)' ,{ 
-  distance: '100px',
-  origin: 'left'
-});
-
-ScrollReveal().reveal('.description-container:nth-of-type(2)' ,{ 
-  distance: '100px',
-  origin: 'right'
-});
-
-// Animating products section
-
-ScrollReveal().reveal('.products-img__container-wrapper:nth-of-type(1)' ,{ 
-  distance: '100px',
-  origin: 'top'
-});
-
-ScrollReveal().reveal('.products-img__container-wrapper:nth-of-type(2)' ,{ 
-  distance: '100px',
-  origin: 'top'
-});
-
-ScrollReveal().reveal('.products__name' ,{ 
-  distance: '100px',
-  origin: 'bottom'
-});
-
-ScrollReveal().reveal('.product-descriptor' ,{ 
-  distance: '100px',
-  origin: 'bottom'
-});
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+})
