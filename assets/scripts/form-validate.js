0619c6b3d66157e03f67de2438d267c5d2bbefbd
Mateus-A-Carvalho <mateusabreu94@hotmail.com>
2024-06-03 23:07:08 -0300

    feat: finished form validating.

--- assets/scripts/form-validate.js
+++ assets/scripts/form-validate.js
@@ -5,29 +5,98 @@ const inputFirstNameEl = document.querySelector('#first-name');
 const inputLastNameEl = document.querySelector('#last-name');
 
 // Input email element; 
-const emailInputel = document.querySelector('#email');
+const emailInputEl = document.querySelector('#email');
 
 // Input datetime and tel elements:
 const inputDatetimeEl = document.querySelector('#input-datetime');
 const inputTelEl = document.querySelector('#input-tel');
 
 /* TODO: 
-  1 - Fazer regex do email;
-  2 - Converter o formato do value do input datetime para mandar o email com a data;
   3 - Fazer regex para o formato de telefone;
 */
+inputFirstNameEl.addEventListener('focus', () => {
+
+})
+
+// Validating form;
 formEl.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log(inputDatetimeEl.value);
-
+  
   if(!inputFirstNameEl.value) {
-    alert('Preencha o seu nome!')    
+    isFirstNameValid(inputFirstNameEl.value);
+    return;
+  }
+  
+  if(!inputLastNameEl.value) {
+    isLastNameValid(inputLastNameEl.value);
     return;
   }
 
-  if(!inputLastNameEl.value) {
-    alert('Preencha o seu nome!');
+  if(!emailInputEl.value) {
+    alert('Please, enter your best email');
     return;    
+  } else if (!isEmailValid(emailInputEl.value)) {
+    alert('Invalid email');
+    return;
+  }
+
+  if(!inputDatetimeEl.value) {
+    isDatetimeValid(inputDatetimeEl.value);
+    return;
+  }
+
+  if(!inputTelEl.value) {
+    alert('Please, enter your phone number!')
+    return;
+  } else if (!isPhoneNumberValid(inputTelEl.value)) {
+    alert('Invalid Phone Number');
+    return;
   }
 
 })
+
+function isEmailValid(email) {
+  // Creating a RegEx to validate email;
+  const emailRegEx = new RegExp(
+    /^[^\s]+@[^\s]+\.[^\s]+$/
+  );
+
+  if(emailRegEx.test(email)) {
+    return true;
+  }
+
+  return false;
+}
+
+function isFirstNameValid(firstName) {
+  if(!firstName) {
+    alert("Fill in your first name!");
+    return true;
+  }
+}
+
+function isLastNameValid(lastName) {
+  if(!lastName) {
+    alert("Fill in your last name!");
+    return true;
+  }
+}
+
+function isDatetimeValid(datetime) {
+  if(!datetime) {
+    alert("Fill in your date time input!");
+    return true;
+  } 
+}
+
+function isPhoneNumberValid(phoneNumber) {
+  const telNumberRegex = new RegExp(
+    /^[1-9][1-9]9\d{4}\d{4}$/
+  );
+
+  if(telNumberRegex.test(phoneNumber)) {
+    return true;
+  }
+
+  return false;
+}
