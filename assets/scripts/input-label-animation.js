d2bb73b112d620646197fdbf7e3b1c657b181b2b
Mateus-A-Carvalho <mateusabreu94@hotmail.com>
2024-06-05 06:42:37 -0300

    feat: added inputFocused feature.

--- assets/scripts/input-label-animation.js
+++ assets/scripts/input-label-animation.js
@@ -10,28 +10,100 @@ import {
 const labelEls = document.querySelectorAll('label');
 
 
-inputFirstNameEl.addEventListener('focus', (e) => {
+inputFirstNameEl.addEventListener('focus', () => {
 
   labelEls.forEach(label => {
     if(inputFirstNameEl.dataset.name === label.dataset.name) {
+      inputFocused(inputFirstNameEl);
       labelAnimate(label);
-      console.log('run')
     }
   })
 });
 
-
-inputFirstNameEl.addEventListener('focusout', (e) => {
+inputFirstNameEl.addEventListener('focusout', () => {
 
   labelEls.forEach(label => {
     if(inputFirstNameEl.dataset.name === label.dataset.name) {
+      inputFocused(inputFirstNameEl);
       labelAnimate(label);
-      console.log('run')
     }
   })
 });
 
-function labelAnimate(label) {
-  return label.classList.toggle('label-animation-focus')
+inputLastNameEl.addEventListener('focus', () => {
+
+  labelEls.forEach(label => {
+    if(inputLastNameEl.dataset.name === label.dataset.name) {
+      inputFocused(inputLastNameEl);
+      labelAnimate(label);
+    }
+  })
+});
+
+inputLastNameEl.addEventListener('focusout', () => {
+
+  labelEls.forEach(label => {
+    if(inputLastNameEl.dataset.name === label.dataset.name) {
+      inputFocused(inputLastNameEl);
+      labelAnimate(label);
+    }
+  })
+});
+
+inputEmailEl.addEventListener('focus', () => {
+
+  labelEls.forEach(label => {
+    if(inputEmailEl.dataset.name === label.dataset.name) {
+      inputFocused(inputEmailEl);
+      labelAnimate(label);
+    }
+  })
+
+})
+
+inputEmailEl.addEventListener('focusout', () => {
+
+  labelEls.forEach(label => {
+    if(inputEmailEl.dataset.name === label.dataset.name) {
+      inputFocused(inputEmailEl);
+      labelAnimate(label);
+    }
+  })
+})
+
+inputTelEl.addEventListener('focus', () => {
+
+  labelEls.forEach(label => {
+    if(inputTelEl.dataset.name === label.dataset.name) {
+      inputFocused(inputTelEl);
+      labelAnimate(label);
+    }
+  })
+
+})
+
+inputTelEl.addEventListener('focusout', () => {
+
+  labelEls.forEach(label => {
+    if(inputTelEl.dataset.name === label.dataset.name) {
+      inputFocused(inputTelEl);
+      labelAnimate(label);
+    }
+  })
+})
+
+inputDatetimeEl.addEventListener('focus', () => {
+  inputFocused(inputDatetimeEl);
+})
+
+inputDatetimeEl.addEventListener('focusout', () => {
+  inputFocused(inputDatetimeEl);
+})
+
+function inputFocused(input) {
+  input.classList.toggle('input-focused')
 }
 
+function labelAnimate(label) {
+  label.classList.toggle('label-animation-focus')
+}
