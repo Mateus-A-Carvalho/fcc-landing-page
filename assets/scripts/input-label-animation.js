9917a6eeb0fcdd67ffae63fd66f34bb0b3401a21
Mateus-A-Carvalho <mateusabreu94@hotmail.com>
2024-06-05 05:40:04 -0300

    feat: working at label animate function. I decided to work with separeted files and ES6 features like export and import.

--- assets/scripts/input-label-animation.js
+++ assets/scripts/input-label-animation.js
@@ -8,10 +8,30 @@ import {
 } from './form-validate.js'
 
 const labelEls = document.querySelectorAll('label');
-console.log(formEl)
 
-formEl.addEventListener('click', (e) => {
-    
 
-})
+inputFirstNameEl.addEventListener('focus', (e) => {
+
+  labelEls.forEach(label => {
+    if(inputFirstNameEl.dataset.name === label.dataset.name) {
+      labelAnimate(label);
+      console.log('run')
+    }
+  })
+});
+
+
+inputFirstNameEl.addEventListener('focusout', (e) => {
+
+  labelEls.forEach(label => {
+    if(inputFirstNameEl.dataset.name === label.dataset.name) {
+      labelAnimate(label);
+      console.log('run')
+    }
+  })
+});
+
+function labelAnimate(label) {
+  return label.classList.toggle('label-animation-focus')
+}
 
